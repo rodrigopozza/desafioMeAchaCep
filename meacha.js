c24a9a634be1a9b443afeffd9caa54565f9ca655
Rodrigo Pozza <dompozza@gmail.com>
2022-02-28 16:46:34 -0300

    desafio me acha cep

---
+++ meacha.js
@@ -0,0 +1,44 @@
+window.onload = function(){
+
+
+  function resultadoCep (dadosCep){
+        for ( let campo in dadosCep){
+            if(document.querySelector(`#${campo}`)){
+                document.querySelector(`#${campo}`).value= dadosCep[campo];
+            }
+        }
+        
+    }
+
+
+  let dadosCep = async (cep) => {
+    let url = `https://viacep.com.br/ws/${cep}/json`;
+    try{
+        let dadosFetch = await fetch(url);
+        let dadosJson =  await dadosFetch.json();
+        resultadoCep(dadosJson)
+    }catch(error){
+        alert(error);
+    }
+   
+   
+            
+}
+
+
+    const btn = document.querySelector('#buscar');
+    const cep = document.querySelector('#cep');
+    const logadouro = document.querySelector('#logadouro');
+    const bairro = document.querySelector('#bairro');
+    const localidade = document.querySelector('#localidade');
+
+     
+       
+  
+   function botao(){ btn.addEventListener('click', function(){
+        dadosCep(cep.value);
+    })     
+} botao()
+}
+
+// export { resultadoCep, dadosCep, btn, cep , localidade, logadouro, bairro, botao};
---
+++ modulomeacha.js
@@ -0,0 +1,10 @@
+import{ resultadoCep, dadosCep, cep, localidade, logadouro, bairro, btn, botao} from "./meacha.js"
+
+resultadoCep(dadosCep);
+dadosCep(cep);
+console.log(cep);
+console.log(localidade);
+console.log(logadouro);
+console.log(bairro);
+console.log(btn);
+botao();
